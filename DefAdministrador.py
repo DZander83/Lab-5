11ed3cc69071ef422d113d28ab2667e41d81a7c4
DAAC07 <d.alvarez0219@gmail.com>
2024-10-16 13:51:20 -0500

    Update DefAdministrador.py

--- DefAdministrador.py
+++ DefAdministrador.py
@@ -5,3 +5,7 @@ Created on Wed Oct 16 12:17:16 2024
 @author: dandr
 """
 def Admin()
+Opciones=print("1: para gestionar estaciones. 2: para gestionar usuarios 3. para volver al menu")
+if Opciones=1
+   GestionEstaciones=print("1: para crear una estacion.","\n", "2: para eliminar una estacion.","\n", " 3: para elegir una estacion."
+if Opciones=2

fb935c02d54bc028977ac83e1809008e2e135667
DAAC07 <d.alvarez0219@gmail.com>
2024-10-16 13:58:21 -0500

    Update DefAdministrador.py

--- DefAdministrador.py
+++ DefAdministrador.py
@@ -5,7 +5,13 @@ Created on Wed Oct 16 12:17:16 2024
 @author: dandr
 """
 def Admin()
-Opciones=print("1: para gestionar estaciones. 2: para gestionar usuarios 3. para volver al menu")
-if Opciones=1
-   GestionEstaciones=print("1: para crear una estacion.","\n", "2: para eliminar una estacion.","\n", " 3: para elegir una estacion."
-if Opciones=2
+print("1: para gestionar estaciones.","\n", " 2: para gestionar usuarios.","\n", " 3. para volver al menu")
+Opciones=input()
+if Opciones=="1":
+   print("1: para crear una estacion.","\n", "2: para eliminar una estacion.","\n", " 3: para elegir una estacion.")
+   GestionEstaciones=input()
+if Opciones=="2":
+   print("1: para crear un usuario.","\n", "2: para editar un usuario.","\n", "para eliminar un usuario")
+   GestionUsuarios=input()
+if Opciones=="3":
+   #algo para que vuelva al menu

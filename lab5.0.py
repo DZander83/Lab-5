9cacddcfea3d994f3201aba77d940ff98a09009e
DAAC07 <d.alvarez0219@gmail.com>
2024-10-23 12:16:17 -0500

    Add files via upload

---
+++ lab5.0.py
@@ -0,0 +1,21 @@
+print("Elija su tipo de usuario o si desea salir del sistema: ","\n", "1. usuario registrado.","\n", "2. usuario visitante.","\n", "3. salir." )
+user=input()
+
+if user == 1:
+    print("Ingrese su nombre y documento:")
+    name=input("Nombre: ")
+    document=input("Documento: ")
+    #funcion que verifique los requsitos de documento y contraseña
+    user=# funcion que verifique si se es admin u operador
+
+elif user == 2:
+    print("Elija el tiempo en el que desea visualizar las estadisticas:", "\n", "1. Ultimos 7 dias.","\n", "2. Ultimos 30 dias.","\n", "3. Elegir manualmente")
+    period=input()
+    if period == 1:
+        # mostrar datos de los ultimos 7 dias
+    elif period == 2:
+        # mostrar los datos de los ultimos 30 dias
+    elif period == 3:
+        input("introduzca una fecha:")
+    
+    

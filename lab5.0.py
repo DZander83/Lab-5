20427e46cc29865e9e72590223b913ba8b814bef
DAAC07 <d.alvarez0219@gmail.com>
2024-10-30 13:22:31 -0500

    Update lab5.0.py

--- lab5.0.py
+++ lab5.0.py
@@ -1,10 +1,18 @@
 import csv y json as cyj
 import DefOperador as DefO
 import DefAdministrador as DefA
+ruta_archivocsvest = 'estaciones.csv'
+ruta_archivocsv = 'usuarios.csv' 
+ruta_archivojson = 'registros.json' 
+ruta_archivojsonvar = 'variables.json'
 print("Elija su tipo de usuario o si desea salir del sistema: ","\n", "1. usuario registrado.","\n", "2. usuario visitante.","\n", "3. salir." )
 user=input()
 
 if user == 1:
+   usuarios = cargar_usuarios(ruta_archivocsv)
+   codigo_ingresado = input("Ingrese su documento: ")
+   clave_ingresada = input("Ingrese su clave: ")
+   resultado = verificar_login(codigo_ingresado, clave_ingresada, usuarios)
    cyj.cargar_usuarios(ruta_archivocsv)
    cyj.verificar_login(codigo, clave, usuarios)
    if seluser==1:

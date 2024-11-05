87715ee97eccc065865c49d2766a3faf838b653c
DZander83 <dilanrf28@gmail.com>
2024-11-05 14:40:57 -0500

    Update utilidades.py

--- utilidades.py
+++ utilidades.py
@@ -204,3 +204,22 @@ def cargar_datos_variables():
     with open("variables.json", mode='r') as variables_json:
         clima_variables = json.load(variables_json)
     return clima_variables
+def Admin()
+print("1: para gestionar estaciones.","\n", " 2: para gestionar usuarios.","\n", " 3. para volver al menu")
+Opciones=input()
+if Opciones=="1":
+   print("1: para crear una estacion.","\n", "2: para eliminar una estacion.","\n", " 3: para elegir una estacion.")
+   GestionEstaciones=input()
+if Opciones=="2":
+   print("1: para crear un usuario.","\n", "2: para editar un usuario.","\n", "para eliminar un usuario")
+   GestionUsuarios=input()
+if Opciones=="3":
+   #algo para que vuelva al menu
+  
+   
+def Operador()
+print("1: para seleccionar una estacion.","\n", " 2. para volver al menu")
+Opciones=input()
+if Opciones=="1":
+
+elif Opciones=="2":
